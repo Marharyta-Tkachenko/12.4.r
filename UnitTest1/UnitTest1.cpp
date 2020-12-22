cdd726cfe0f734e7d07fe9e43a9e885eb98d22df
Marharyta-Tkachenko <margoshyn23@gmail.com>
2020-12-22 02:22:11 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project12.4.r/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Spusok* L = NULL;
+			Spusok* L1 = NULL;
+			Spusok* L2 = NULL;
+			Spusok* T, * G;
+
+			int x = 0, inf, N = 2, i = 0;
+			T = L;
+			inf = -3;
+			Creata(L, T, inf, N, i);
+
+			T = L;
+			Process(L, L1, L2, T, G, i);
+
+			T = L2;
+			int l = T->inf;
+			Assert::AreEqual(l, -3);
+		}
+	};
+}
